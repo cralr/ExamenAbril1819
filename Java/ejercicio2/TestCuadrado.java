082f3a63057eaa98374aa31d6af822fe63af1a47
cralr <rafamcruza10@gmail.com>
2019-04-28 18:03:56 +0200

    Modificacion en TestCuadrado

--- Java/ejercicio2/TestCuadrado.java
+++ Java/ejercicio2/TestCuadrado.java
@@ -13,23 +13,26 @@ import ejercicio2.utiles.Teclado;
 public class TestCuadrado {
 
   public static void main(String[] args) throws NoEsEnteroException {
-    Cuadrado cuadrado1 = new Cuadrado(7);
-    Cuadrado cuadrado2 = new Cuadrado(6);
     
-    System.out.println("Cuadrado1");
-    System.out.println(cuadrado1);
-    
-    System.out.println("Cuadrado2");
-    System.out.println(cuadrado2);
-    
-    if (cuadrado1.compareTo(cuadrado2)==0) {
-      System.out.println("Son iguales.");
-    } else if(cuadrado1.compareTo(cuadrado2)<0){
-      System.out.println("Cuadrado1 menor a Cuadrado2");
-    }else {
-      System.out.println("Cuadrado1 mayor a Cuadrado2");
-    }
+    try {
+      Cuadrado cuadrado1 = new Cuadrado(12);
+      Cuadrado cuadrado2 = new Cuadrado(6);
+      System.out.println("Cuadrado1");
+      System.out.println(cuadrado1);
 
+      System.out.println("Cuadrado2");
+      System.out.println(cuadrado2);
+
+      if (cuadrado1.compareTo(cuadrado2) == 0) {
+        System.out.println("Son iguales.");
+      } else if (cuadrado1.compareTo(cuadrado2) < 0) {
+        System.out.println("Cuadrado1 menor a Cuadrado2");
+      } else {
+        System.out.println("Cuadrado1 mayor a Cuadrado2");
+      }
+    } catch (ArithmeticException ae) {
+      System.err.println("No se puede crear el cuadrado, valores mal introducidos.");
+    }
   }
 
 }
