7dd389c6c82a38cdb9763b3270bd85fa298eee2a
cralr <rafamcruza10@gmail.com>
2019-05-09 18:11:14 +0200

    Indentacion modificada close #1

--- Java/ejercicio1/utiles/Teclado.java
+++ Java/ejercicio1/utiles/Teclado.java
@@ -15,116 +15,120 @@ import ejercicio1.almacen.exceptions.NoEsEnteroException;
  */
 public class Teclado {
 
-	static Scanner entrada = new Scanner(System.in);
+  static Scanner entrada = new Scanner(System.in);
 
-	/**
-	 * Lee un carácter del teclado
-	 * 
-	 * @return carácter introducido por el usuario
-	 * @throws IOException
-	 */
-	public static char leerCaracter()  {
-		char caracter;
-		try {
-			return leerCadena().charAt(0);
-		} catch (Exception e) {
-			caracter = 0;
-		}
-		return caracter;
-	}
+  /**
+   * Lee un carácter del teclado
+   * 
+   * @return carácter introducido por el usuario
+   * @throws IOException
+   */
+  public static char leerCaracter() {
+    char caracter;
+    try {
+      return leerCadena().charAt(0);
+    } catch (Exception e) {
+      caracter = 0;
+    }
+    return caracter;
+  }
 
-	/**
-	 * Lee un carácter del teclado
-	 * 
-	 * @param mensaje mensaje mostrado al usuario
-	 * @return carácter introducido por el usuario
-	 * @throws IOException
-	 */
-	public static char leerCaracter(String mensaje)  {
-		System.out.println(mensaje);
-		return leerCaracter();
-	}
+  /**
+   * Lee un carácter del teclado
+   * 
+   * @param mensaje
+   *          mensaje mostrado al usuario
+   * @return carácter introducido por el usuario
+   * @throws IOException
+   */
+  public static char leerCaracter(String mensaje) {
+    System.out.println(mensaje);
+    return leerCaracter();
+  }
 
-	/**
-	 * Lee una cadena del teclado
-	 * 
-	 * @param mensaje mensaje mostrado al usuario
-	 * @return cadena introducida por el usuario
-	 * @throws IOException
-	 */
-	public static String leerCadena(String mensaje) {
-		System.out.println(mensaje);
-		return leerCadena();
-	}
+  /**
+   * Lee una cadena del teclado
+   * 
+   * @param mensaje
+   *          mensaje mostrado al usuario
+   * @return cadena introducida por el usuario
+   * @throws IOException
+   */
+  public static String leerCadena(String mensaje) {
+    System.out.println(mensaje);
+    return leerCadena();
+  }
 
-	/**
-	 * Lee una cadena del teclado
-	 * 
-	 * @return cadena introducida por el usuario
-	 * 
-	 */
+  /**
+   * Lee una cadena del teclado
+   * 
+   * @return cadena introducida por el usuario
+   * 
+   */
 
-	public static String leerCadena() {
-	  return entrada.nextLine();
+  public static String leerCadena() {
+    return entrada.nextLine();
 
-	}
+  }
 
-	/**
-	 * Lee un entero del teclado
-	 * 
-	 * @return entero introducido por el usuario
+  /**
+   * Lee un entero del teclado
+   * 
+   * @return entero introducido por el usuario
+   * 
+   * @throws NoEsEnteroException
+   */
+  public static int leerEntero() throws NoEsEnteroException {
 
-	 * @throws NoEsEnteroException 
-	 */
-	public static int leerEntero() throws NoEsEnteroException {
-	
-		try {
-			return Integer.parseInt(leerCadena().trim()); 
-		} catch (NumberFormatException e) {
-  			throw new NoEsEnteroException("Introduzca un número entero.");
-		}
-	}
+    try {
+      return Integer.parseInt(leerCadena().trim());
+    } catch (NumberFormatException e) {
+      throw new NoEsEnteroException("Introduzca un número entero.");
+    }
+  }
 
-	/**
-	 * Lee una entero del teclado
-	 * 
-	 * @param mensaje mensaje mostrado al usuario
-	 * @return entero introducida por el usuario
+  /**
+   * Lee una entero del teclado
+   * 
+   * @param mensaje
+   *          mensaje mostrado al usuario
+   * @return entero introducida por el usuario
+   * 
+   * @throws NoEsEnteroException
+   */
+  public static int leerEntero(String mensaje) throws NoEsEnteroException {
+    System.out.println(mensaje);
+    return leerEntero();
+  }
 
-	 * @throws NoEsEnteroException 
-	 */
-	public static int leerEntero(String mensaje) throws NoEsEnteroException {
-		System.out.println(mensaje);
-		return leerEntero();
-	}
-
-	/**
-	 * Lee un decimal del teclado
-	 * 
-	 * @return decimal introducido por el usuario
-	
-	 * @throws NoEsDecimalException 
-	 */
-	public static double leerDecimal() throws NoEsDecimalException {
-		try {
-			return Double.parseDouble(leerCadena().trim()); 
-		} catch (NumberFormatException e) {
+  /**
+   * Lee un decimal del teclado
+   * 
+   * @return decimal introducido por el usuario
+   * 
+   * @throws NoEsDecimalException
+   */
+  public static double leerDecimal() throws NoEsDecimalException {
+    try {
+      return Double.parseDouble(leerCadena().trim());
+    } catch (NumberFormatException e) {
       throw new NoEsDecimalException("Introduzca un número decimal.");
     }
-	}
+  }
 
-	/**
-	 * Lee una decimal del teclado
-	 * 
-	 * @param mensaje mensaje mostrado al usuario
-	 * @return decimal introducida por el usuario
-	 * @throws IOException
-	 * @throws NumberFormatException
-	 * @throws NoEsDecimalException 
-	 */
-	public static double leerDecimal(String mensaje) throws NoEsDecimalException {
-		System.out.println(mensaje);
-		return leerDecimal();
-	}
+  /**
+   * Lee una decimal del teclado
+   * 
+   * @param mensaje
+   *          mensaje mostrado al usuario
+   * @return decimal introducida por el usuario
+   * @throws IOException
+   * @throws NumberFormatException
+   * @throws NoEsDecimalException
+   */
+  public static double leerDecimal(String mensaje) throws NoEsDecimalException {
+    System.out.println(mensaje);
+    return leerDecimal();
+  }
 
 }
